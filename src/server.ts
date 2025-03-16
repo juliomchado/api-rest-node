ba6d2439f854ddef4174850c4681311a9b778e2f
Julio Machado <juliocarlos00@hotmail.com>
2025-03-16 01:08:27 +0000

    first commit with knex and sqlite

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+import { knex } from "./database";
+
+const app = fastify();
+
+app.get("/hello", async () => {
+  const tables = await knex("sqlite_schema").select("*");
+
+  return tables;
+});
+
+app
+  .listen({
+    port: 3334,
+  })
+  .then(() => {
+    console.log("HTTP server is running in port 3334");
+  });
